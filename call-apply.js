1e82375ebb5bf083e5837862410d2535253526ab
mathew94 <mathewrodrick94@hotmail.com>
2020-08-12 00:38:13 +0600

    bind

--- call-apply.js
+++ call-apply.js
@@ -6,13 +6,36 @@ const normalPerson = {
         console.log(this.firstName, this.lastName);
     },
     chargeBill: function(amount){
+        console.log(this);
         this.salary = this.salary - amount;
         return this.salary;
     }
 
 }
 
-normalPerson.chargeBill(150);
-normalPerson.chargeBill(850);
-normalPerson.chargeBill(2000);
-console.log(normalPerson.salary);
+// normalPerson.chargeBill(150);
+// normalPerson.chargeBill(850);
+// normalPerson.chargeBill(2000);
+// console.log(normalPerson.salary);
+
+const heroPerson = {
+    firstName: "Hero",
+    lastName: "Balam",
+    salary: 25000,
+}
+
+const friendlyPerson = {
+    firstName: "Hero",
+    lastName: "Golam",
+    salary: 25000,
+}
+
+const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+heroChargeBill(2000);
+heroChargeBill(3000);
+console.log(heroPerson.salary);
+
+const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+friendlyChargeBill(3000);
+friendlyChargeBill(5000);
+console.log(friendlyPerson.salary);
