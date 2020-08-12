7784323acc048ff622e38879290091aba765c42b
mathew94 <mathewrodrick94@hotmail.com>
2020-08-13 01:20:37 +0600

    Complete

--- call-apply.js
+++ call-apply.js
@@ -5,9 +5,9 @@ const normalPerson = {
     getFullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount, tips, tax){
         console.log(this);
-        this.salary = this.salary - amount;
+        this.salary = this.salary - amount - tips - tax;
         return this.salary;
     }
 
@@ -30,12 +30,24 @@ const friendlyPerson = {
     salary: 25000,
 }
 
-const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
-heroChargeBill(2000);
-heroChargeBill(3000);
-console.log(heroPerson.salary);
+////////////bind///////////////
+// const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+// heroChargeBill(2000);
+// heroChargeBill(3000);
+// console.log(heroPerson.salary);
 
-const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
-friendlyChargeBill(3000);
-friendlyChargeBill(5000);
-console.log(friendlyPerson.salary);
+// const friendlyChargeBill = normalPerson.chargeBill.bind(friendlyPerson);
+// friendlyChargeBill(3000);
+// friendlyChargeBill(5000);
+// console.log(friendlyPerson.salary);
+
+////////call///////////
+// normalPerson.chargeBill.call(heroPerson, 900, 100, 25);
+// normalPerson.chargeBill.call(heroPerson, 3000, 300, 30);
+// normalPerson.chargeBill.call(friendlyPerson, 6000, 500, 100);
+// console.log(friendlyPerson.salary);
+// console.log(heroPerson.salary);
+
+/////////////apply///////////
+normalPerson.chargeBill.apply(heroPerson, [3000, 400, 100]);
+console.log(heroPerson.salary);
---
+++ timeout.js
@@ -0,0 +1,13 @@
+function doSomething(){
+    console.log(3333);
+}
+console.log(2222);
+// setTimeout(() => {
+//     console.log("See you later")
+// }, 4000);
+
+setInterval(function(){
+    console.log("doing it");
+}, 2000);
+
+console.log(4444);
